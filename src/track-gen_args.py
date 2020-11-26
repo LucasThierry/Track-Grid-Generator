dd6bd5ff921849368c2bf253e45b62f44d67a32f
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2020-11-26 00:12:36 -0300

    Preset scaling fixed to args size

--- src/track-gen_args.py
+++ src/track-gen_args.py
@@ -40,51 +40,58 @@ def run():
             grid[row].append(" ")
 
     # Setup initial asphalt tiles
+
+    s = args.size[0]
+    s2 = int(s/2)
+    s4 = int(s/4)
+    print(s)
+    print(s2)
+
     if args.preset == 'straight':
-        grid [3][0] = "☐"
-        grid [4][0] = "☐"
-        grid [3][3] = "☐"
-        grid [4][4] = "☐"
-        grid [3][7] = "☐"
-        grid [4][7] = "☐"
+        grid [s2-1][0] = "☐"
+        grid [s2][0] = "☐"
+        grid [s2-1][s2-1] = "☐"
+        grid [s2][s2] = "☐"
+        grid [s2-1][s-1] = "☐"
+        grid [s2][s-1] = "☐"
             
     elif args.preset == '90':
-        grid [0][3] = "☐"
-        grid [0][4] = "☐"
-        grid [3][3] = "☐"
-        grid [4][4] = "☐"
-        grid [3][7] = "☐"
-        grid [4][7] = "☐"
+        grid [0][s2-1] = "☐"
+        grid [0][s2] = "☐"
+        grid [s2-1][s2-1] = "☐"
+        grid [s2][s2] = "☐"
+        grid [s2-1][s-1] = "☐"
+        grid [s2][s-1] = "☐"
 
     elif args.preset == '45':
-        grid [0][1] = "☐"
-        grid [0][2] = "☐"
-        grid [3][3] = "☐"
-        grid [4][4] = "☐"
-        grid [5][7] = "☐"
-        grid [6][7] = "☐"
+        grid [0][s4-1] = "☐"
+        grid [0][s4-2] = "☐"
+        grid [s2-1][s2-1] = "☐"
+        grid [s2][s2] = "☐"
+        grid [s4+s2-3][s-1] = "☐"
+        grid [s4+s2-2][s-1] = "☐"
 
     elif args.preset == 'random':
         grid [0][0] = "☐"
         grid [0][1] = "☐"
         grid [1][0] = "☐"
-        grid [7][7] = "☐"
-        grid [6][7] = "☐"
-        grid [7][6] = "☐"
-        grid [0][7] = "☐"
-        grid [1][7] = "☐"
-        grid [0][6] = "☐"
-        grid [7][0] = "☐"
-        grid [7][1] = "☐"
-        grid [6][0] = "☐"
+        grid [s-1][s-1] = "☐"
+        grid [s-2][s-1] = "☐"
+        grid [s-1][s-2] = "☐"
+        grid [0][s-1] = "☐"
+        grid [1][s-1] = "☐"
+        grid [0][s-2] = "☐"
+        grid [s-1][0] = "☐"
+        grid [s-1][1] = "☐"
+        grid [s-2][0] = "☐"
 
     elif args.preset == 'dia':
         grid [0][0] = "☐"
         grid [0][1] = "☐"
         grid [1][0] = "☐"
-        grid [7][7] = "☐"
-        grid [6][7] = "☐"
-        grid [7][6] = "☐"
+        grid [s-1][s-1] = "☐"
+        grid [s-2][s-1] = "☐"
+        grid [s-1][s-2] = "☐"
 
     elif args.preset == 'custom':
         q = int(input("Quantos pontos inicias? "))
@@ -98,12 +105,12 @@ def run():
     for asphalts in range(args.density[0]):
         c_x = randint(1, len(grid)-2)
         c_y = randint(1, len(grid[0])-2)
-        while (grid[c_x+1][c_y] == " ") and (grid[c_x][c_y+1] == " ") and (grid[c_x-1][c_y] == " ") and (grid[c_x][c_y-1] == " "):
+        while (grid[c_x+1][c_y] == " ") and (grid[c_x][c_y+1] == " ") and (grid[c_x-1][c_y] == " ") and (grid[c_x][c_y-1] == " ") and (grid[c_x][c_y] == "☐"):
             c_x = randint(0, len(grid) - 2)
             c_y = randint(0, len(grid[0]) - 2)
         grid[c_x][c_y] = "☐"
 
-    for n in range (8):
+    for n in range (s):
         print(grid[n])
 
 if __name__ == '__main__':
